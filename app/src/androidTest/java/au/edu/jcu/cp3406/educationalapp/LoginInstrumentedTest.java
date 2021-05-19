0db2c53dfe336d3b8f53543317b07fcf0f2a00a7
Hayden-Bailey <hayden.bailey@my.jcu.edu.au>
2021-05-20 07:50:06 +1000

    Create LoginInstrumentedTest.java
    Add code for testing the login functionality through test context

--- app/src/androidTest/java/au/edu/jcu/cp3406/educationalapp/LoginInstrumentedTest.java
+++ app/src/androidTest/java/au/edu/jcu/cp3406/educationalapp/LoginInstrumentedTest.java
@@ -2,9 +2,12 @@ package au.edu.jcu.cp3406.educationalapp;
 
 import android.content.Context;
 
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -16,11 +19,33 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class LoginInstrumentedTest {
+    private LoginDatabaseHelper loginDatabaseHelper;
+
+    @Before
+    public void createDB() {
+        Context context = ApplicationProvider.getApplicationContext();
+        loginDatabaseHelper = new LoginDatabaseHelper(context);
+    }
+
+    @After
+    public void closeDB() {
+    }
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("au.edu.jcu.cp3406.educationalapp", appContext.getPackageName());
+    }
+
+    @Test
+    public void loginEmailExists() {
+
+        loginDatabaseHelper.addLoginDetails("example@example.com", "example");
+
+        assertTrue(loginDatabaseHelper.checkEmails("example@example.com"));
+        assertFalse(loginDatabaseHelper.checkEmails("test@example.com"));
+
     }
 }
