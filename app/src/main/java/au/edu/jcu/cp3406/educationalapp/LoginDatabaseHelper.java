9ea4c25484f71750f45a28752e4ae7d9fc690f15
Hayden-Bailey <hayden.bailey@my.jcu.edu.au>
2021-05-20 03:04:10 +1000

    Update LoginDatabaseHelper.java
    Update database helper code to add emails from xml

--- app/src/main/java/au/edu/jcu/cp3406/educationalapp/LoginDatabaseHelper.java
+++ app/src/main/java/au/edu/jcu/cp3406/educationalapp/LoginDatabaseHelper.java
@@ -1,23 +1,49 @@
 package au.edu.jcu.cp3406.educationalapp;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 
 public class LoginDatabaseHelper extends SQLiteOpenHelper {
 
-    public LoginDatabaseHelper( Context context) {
-        super(context, TABLE_NAME, null);
+    private static final String TAG = LoginDatabaseHelper.class.getSimpleName();
+
+    private static final String TABLE_NAME = "loginDetails";
+    private static final String COL1 = "id";
+    private static final String COL2 = "email";
+    private static final String COL3 = "password";
+
+
+    public LoginDatabaseHelper(Context context) {
+        super(context, TABLE_NAME, null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        String createTable = "CREATE TABLE " + TABLE_NAME + " (id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COL2 + COL3 + " TEXT)";
+        db.execSQL(createTable);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
 
+    public boolean addLoginDetails(String email, String password) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL2, email);
+        contentValues.put(COL3, password);
+
+        Log.i(TAG, "addLoginDetails adding: email - " + email + " password - " + password + " to " + TABLE_NAME);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+
+        return result != -1;
     }
 }
